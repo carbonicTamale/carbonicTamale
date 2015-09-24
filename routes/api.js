6bd92355a9dc63ba72e7ebb7f3b93329a1740d02
Marcus Buffett <marcusbuffett@me.com>
2015-09-24 15:12:59 -0700

    Some API work

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,25 @@ var config = require('../config');
 var router = express.Router();
 var verify = require('./verify');
 
-router.get('/users/addFriend', verify, function(req, res, next) {
+router.post('/users/friends', verify, function(req, res, next) {
   var user = req.user.username;
   var friend = req.body.friend;
-  //DB stuff
+  // TODO : add the friend to the user's friend list, and vice versa
   res.send('success');
 });
+
+
+router.post('/jams', function(req, res, next) {
+  // TODO : return a unique jam id, add to DB
+  res.send('jam_id');
+});
+
+router.post('/jams/:jam/users', function(req, res, next) {
+  var user = req.body.user;
+  // TODO : invite the user
+  res.send('Invitation successful');
+});
+
+
 
 module.exports = router;
