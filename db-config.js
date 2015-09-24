678abfb977148f4cd1925fc860a829f3dce16ade
Matthew Brooks <mdeanbrooks@gmail.com>
2015-09-23 20:02:41 -0700

    Remove instrument model

--- db-config.js
+++ db-config.js
@@ -16,15 +16,7 @@ var User = sequelize.define('User', {
 //set up user to user relation as friends
 User.hasMany(User, { as: 'Friend' });
 
-//define instrument model
-var Instrument = sequelie.define('Instrument', {
-  name: sequelize.STRING,
-  midi: sequelize.TEXT
-});
-
 //create tables if there are none
 User.sync();
-Instrument.sync();
 
 exports.User = User;
-exports.Instrument = Instrument;
