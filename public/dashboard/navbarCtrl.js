51fffdeb63e7e9224624531ad6dcc47a624d8805
Chris Yung <cyung@bu.edu>
2015-09-29 19:15:28 -0700

    create toast message template

--- public/dashboard/navbarCtrl.js
+++ public/dashboard/navbarCtrl.js
@@ -1,36 +1,45 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	angular.module('app')
-	.controller('NavbarCtrl', navbarCtrl);
+  angular.module('app')
+  .controller('NavbarCtrl', navbarCtrl);
 
-	function navbarCtrl($state) {
-		var self = this;
-		self.state = $state;
+  function navbarCtrl($state, $mdToast) {
+    var self = this;
 
-		self.menu = [
-			{
-				link: 'dashboard',
-				title: 'Dashboard',
-				icon: 'dashboard'
-			},
-			{
-				link: 'friends',
-				title: 'Friends',
-				icon: 'group'
-			},
-			{
-				link: 'jam',
-				title: 'Jam',
-				icon: 'create'
-			}
-		];
+    self.menu = [
+      {
+        link: 'dashboard',
+        title: 'Dashboard',
+        icon: 'dashboard'
+      },
+      {
+        link: 'friends',
+        title: 'Friends',
+        icon: 'group'
+      },
+      {
+        link: 'jam',
+        title: 'Jam',
+        icon: 'create'
+      }
+    ];
 
-		self.isSelected = function(title) {
-			if (!self.state.current.hasOwnProperty('data'))
-				return false;
-			
-			return (title === self.state.current.data.title);
-		}
-	}
+    self.isSelected = function(title) {
+      if (!$state.current.hasOwnProperty('data'))
+        return false;
+      
+      return (title === $state.current.data.title);
+    };
+
+    self.showToast = function() {
+      $mdToast.show({
+        templateUrl: 'dashboard/toast-invite.html',
+        hideDelay: 0,
+        position: 'bottom right',
+        controller: 'ToastCtrl',
+        controllerAs: 'toast'
+      });
+    }
+  }
 })();
