9587de85ff1093fb56572566722827d4aff7a452
bkillenit <blaine.j.killen@gmail.com>
2015-10-01 11:22:59 -0700

    basic integration of docco included

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,24 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-docco');
   grunt.initConfig({
-  
+
     mochaTest: {
       testTest: {
         src: ['test/**/*.js']
       }
+    },
+
+    docco: {
+      debug: {
+        src: ['./*/*.js', './*.js'],
+        options: {
+          output: 'docs/'
+        }
+      }
     }
   });
+
   grunt.registerTask('test', ['mochaTest:testTest']);
+  grunt.registerTask('docs', ['docco']);
 };
