6ffc19a034d09b29944b1bdad4ceac574995cb8a
Marcus Buffett <marcusbuffett@me.com>
2015-10-01 15:47:50 -0400

    Compression middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var passport = require('passport');
 var session = require('express-session');
 var db = require('./config/db-config');
+var compress = require('compression');
 
 
 var routes = require('./routes/index');
@@ -17,6 +18,7 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(compress());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
